90b1bd140cc60521558b41a19504b86d412e15ec
katianemiranda <katianemiranda00@gmail.com>
2026-03-05 22:44:13 -0300

    mais

--- internal/entity/bid_entity/bid_entity.go
+++ internal/entity/bid_entity/bid_entity.go
@@ -1,6 +1,11 @@
 package bid_entity
 
-import "time"
+import (
+	"context"
+	"time"
+
+	"github.com/katianemiranda/leilao/internal/internal_error"
+)
 
 type Bid struct {
 	ID        string
@@ -8,4 +13,14 @@ type Bid struct {
 	AuctionID string
 	Amount    float64
 	Timestamp time.Time
+}
+
+type BidEntityRepository interface {
+	CreateBid(ctx context.Context, bidEntities []Bid) *internal_error.InternalError
+
+	FindBidAndAuctionById(ctx context.Context, id string) (*Bid, *internal_error.InternalError)
+
+	FindBidsByAuctionId(ctx context.Context, auctionId string) ([]*Bid, *internal_error.InternalError)
+
+	FindWinningBidByAuctionId(ctx context.Context, auctionId string) (*Bid, *internal_error.InternalError)
 }
